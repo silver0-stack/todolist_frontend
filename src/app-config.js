9d0d0027f65a65c3edce9790e6179c91866e7cef
최은영 <76518850+silver0-stack@users.noreply.github.com>
2023-04-10 03:42:11 +0900

    주소명 오타 검사

--- src/app-config.js
+++ src/app-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else{
-  backendHost="https://api.wat-to-do.com/"
+  backendHost="https://api.wat-to-do.com"
 }
 
 export const API_BASE_URL = `${backendHost}`;
